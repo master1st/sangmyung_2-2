9b506dcee49b2093bb233f2730c79653df3fe0c8
seokhyun <gah1234@naver.com>
2021-11-28 19:35:28 +0900

    기말시험공부

---
+++ 객체지향프로그래밍/파일입출력 및 GUI 프로그래밍 기말시험/MouseListenerEx.java
@@ -0,0 +1,39 @@
+package 기말시험준비;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MouseListenerEx extends JFrame{
+	private JLabel la = new JLabel("Hello");
+	public MouseListenerEx() {
+		super("Mouse 이벤트 예제");
+		setSize(300,400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		setVisible(true);
+		c.setLayout(null);
+		
+		c.addMouseListener(new MyMouseListener());
+		la.setSize(50,20);
+		la.setLocation(30,30);
+		c.add(la);
+	}
+	
+
+	
+	class MyMouseListener implements MouseListener{
+		public void mousePressed(MouseEvent e) {
+			int x = e.getX();
+			int y = e.getY();
+			la.setLocation(x,y);
+			
+		}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseClicked(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		
+	}
+	public static void main(String[] args) {
+		new MouseListenerEx();
+	}
+}
