8ed4a7b79766349364d29a2335ba3b794384f1ce
seokhyun <gah1234@naver.com>
2021-12-05 12:56:20 +0900

    입출력

---
+++ 객체지향프로그래밍/8장 예제 java 파일/FileInputStreamEx.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+
+public class FileInputStreamEx {
+	public static void main(String[] args) {
+		byte b[] = new byte[6];
+		try {
+			FileInputStream fin = new FileInputStream("‪‪‪C:\\Users\\82109\\Desktop\\test.txt");
+			int n=0,c;
+			while((c = fin.read())!= -1) {
+				b[n] = (byte)c;
+				n++;
+			}
+			System.out.println("‪C:\\Users\\82109\\Downloads\\test.txt에서 읽은 배열을 출력합니다.");
+			for(int i=0; i<b.length; i++) 
+				System.out.print(b[i] + " ");
+				System.out.println();
+				
+				fin.close();
+			} catch(IOException e) {
+				System.out.println("‪C:\\Users\\82109\\Downloads\\test.txt에서 읽지 못했습니다. 경로명을 체크해보세요");
+				
+			}
+		}
+	}
+
+
